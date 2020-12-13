5f47de475f21c9801f2b086bd078f7403a0426cc
billtubbs <bill.tubbs@me.com>
2020-12-13 11:28:57 -0800

    Analog input test

--- test_analog_in/test_analog_in.ino
+++ test_analog_in/test_analog_in.ino
@@ -11,16 +11,24 @@ int boardLed = 13;
 // Pin where LED 1 is connected
 int led = 2;
 
+// Pin where analog input is connected
+int a_in = A0;
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
 }
 
+float a_in_value;
+
 // the loop routine runs over and over again forever:
 void loop() {
+  a_in_value = analogRead(a_in);
+  Serial.print("analog input is: ");
+  Serial.println(a_in_value);
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);               // wait for 200 milliseconds
+  delay(a_in_value);               // wait for 200 milliseconds
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(800);               // wait for 800 milliseconds
+  delay(a_in_value);               // wait for 800 milliseconds
 }
